e1df010db13267fb8525a27195aed1686a7ed155
jason5014 <1696732332@qq.com>
2019-02-19 10:00:18 +0800

    项目结构调整，修改路由和配置信息

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import routes from './router'
+import VueRouter from 'vue-router'
+import { routerMode } from '../config/env'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes,
+  mode: routerMode,
+  strict: process.env.NODE_ENV !== 'production'
+})
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import App from '../App'
 
-Vue.use(Router)
+// const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+export default [
+  {
+    path: '/',
+    component: App
+    // children: [
+    //   {
+    //     path: '',
+    //     redirect: '/home'
+    //   },
+    //   {
+    //     path: '/home',
+    //     component: home
+    //   }
+    // ]
+  }
+]
